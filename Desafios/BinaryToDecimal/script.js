c92e4d1826af3f02bbec1d7b6c5e790bff4b0249
Candido Luiz <candidoluiz097@gmail.com>
2020-12-05 17:20:11 -0300

    Melhorando o CSS do conversor

--- Desafios/BinaryToDecimal/script.js
+++ Desafios/BinaryToDecimal/script.js
@@ -1,12 +1,22 @@
 
 let btn = document.getElementById("btn");
+let input = document.getElementById("binario");
 btn.addEventListener("click",converter);
-
-
+input.addEventListener("input",verificaNumero);
 
 function converter(){
     let bin = document.getElementById("binario");
     let dec = document.getElementById("decimal");
     let binario = bin.value;
     dec.value = parseInt(binario,2);
+}
+
+function verificaNumero(event){
+    if (event.data != "1" && event.data != "0" && event.inputType != "deleteContentBackward"){
+        alert("Digite apenas 1 ou 0!");
+        
+        input.value = input.value;
+        let lastNumber = (input.value.length)-1;
+        input.value = input.value.replace(input.value[lastNumber], "");
+    }
 }
