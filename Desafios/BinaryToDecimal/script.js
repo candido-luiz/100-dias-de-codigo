59201afe340caab7b9fc52874ae7a73af3069b9b
Candido Luiz <candidoluiz097@gmail.com>
2020-12-06 11:42:08 -0300

    eliminando redundancias do js

--- Desafios/BinaryToDecimal/script.js
+++ Desafios/BinaryToDecimal/script.js
@@ -1,13 +1,10 @@
 
-let btn = document.getElementById("btn");
 let input = document.getElementById("binario");
-btn.addEventListener("click",converter);
 input.addEventListener("input",verificaNumero);
 
 function converter(){
-    let bin = document.getElementById("binario");
     let dec = document.getElementById("decimal");
-    let binario = bin.value;
+    let binario = input.value;
     dec.value = parseInt(binario,2);
 }
 
@@ -15,7 +12,6 @@ function verificaNumero(event){
     if (event.data != "1" && event.data != "0" && event.inputType != "deleteContentBackward"){
         alert("Digite apenas 1 ou 0!");
         
-        input.value = input.value;
         let lastNumber = (input.value.length)-1;
         input.value = input.value.replace(input.value[lastNumber], "");
     }
