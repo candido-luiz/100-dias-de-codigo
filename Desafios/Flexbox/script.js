0484688e3ab9e3946a7f9f058d7b4388bf2cdd07
Candido Luiz <candidoluiz097@gmail.com>
2020-12-14 18:46:55 -0300

    Atualizando o script

--- Desafios/Flexbox/script.js
+++ Desafios/Flexbox/script.js
@@ -2,3 +2,16 @@ function trocar(p){
     let ul = p.parentElement.children[1];
     ul.classList.toggle("toggle");
 }
+let flexContainer = document.getElementById("flex-container");
+
+function carregar(){
+    let li = document.getElementsByTagName("li");
+    for(option of li){
+    option.addEventListener("click",addStyle);
+}
+}
+
+
+function addStyle(){
+    console.log(this.children[0]);
+}
