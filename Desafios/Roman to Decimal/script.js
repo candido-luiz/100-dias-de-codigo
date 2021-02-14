1980d55e92914a47bff7a2e783f3a88e0831a54f
Candido Luiz <candidoluiz097@gmail.com>
2021-02-13 22:56:37 -0300

    Pattern do regexp completo

--- Desafios/Roman to Decimal/script.js
+++ Desafios/Roman to Decimal/script.js
@@ -1,11 +1,12 @@
-let algarismo = "XXXIICCMMIII";
+let algarismo = "";
 
-let pattern_1 = /(I{4}|X{4}|C{4}|M{4})/i;
-let pattern_2 = /((I{2}X)|(I(L|C|D|M)))/i;
-let pattern_3 = /((X{2}(L|C))|(X(D|M)))/i;
-let pattern_4 = /C{2}(D|M)/i;
-let pattern_5 = /V(X|L|C|D|M)/i;
-let pattern_6 = /L(C|D|M)/i;
-let pattern_7 = /DM/i;
+// let pattern_1 = /(I{4}|X{4}|C{4}|M{4})/i;
+// let pattern_2 = /((I{2}X)|(I(L|C|D|M)))/i;
+// let pattern_3 = /((X{2}(L|C))|(X(D|M)))/i;
+// let pattern_4 = /C{2}(D|M)/i;
+// let pattern_5 = /V(X|L|C|D|M|V)/i;
+// let pattern_6 = /L(C|D|M|L)/i;
+// let pattern_7 = /D(M|D)/i;
 
-console.log(pattern.test(algarismo));
+let fullPattern = /((I{4}|X{4}|C{4}|M{4})|((I{2}X)|(I(L|C|D|M)))|((X{2}(L|C))|(X(D|M)))|C{2}(D|M)|V(X|L|C|D|M|V)|L(C|D|M|L)|D(M|D))/i;
+console.log(fullPattern.test(algarismo));
