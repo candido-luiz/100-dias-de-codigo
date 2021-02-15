e6b91e97d8fc242aad9f9c028aaea0451b12efed
Candido Luiz <candidoluiz097@gmail.com>
2021-02-15 15:19:46 -0300

    Terminando o sistema de conversão

--- Desafios/Roman to Decimal/script.js
+++ Desafios/Roman to Decimal/script.js
@@ -1,4 +1,4 @@
-let algarismo = "CMXCIX";
+valorConvertido = document.querySelector("#decimal");
 
 let P1 = "(I{4}|X{4}|C{4}|M{4})"
 let P2 = "((I{2}X)|(I(L|C|D|M)))"
@@ -10,26 +10,43 @@ let P7 = "D(M|D)"
 let P8 = "I[^I]+I"
 let P9 = "(C[^C]+CC|C[^C]+C[^C]+C)"
 let P10 = "V[^V]+V"
-let P11 = "D[^D]+D"
-let P12 = "M[^M]+M"
-let P13 = "(X[^X]+XX|X[^X]+X[^X]+X)"
+let P11 = "L[^L]+L"
+let P12 = "D[^D]+D"
+let P13 = "M[^M]+M"
+let P14 = "(X[^X]+XX|X[^X]+X[^X]+X)"
 
-let pattern = `(${P1}|${P2}|${P3}|${P4}|${P5}|${P6}|${P7}|${P8}|${P9}|${P10}|${P11}|${P12}|${P13})`;
+let full_P = `(${P1}|${P2}|${P3}|${P4}|${P5}|${P6}|${P7}|${P8}|${P9}|${P10}|${P11}|${P12}|${P13}|${P14})`;
+let pattern = new RegExp(full_P, "i");
 
-let tamanhoAlgarismo = algarismo.length - 1;
-let i;
-let valorAnterior = 0;
-let soma = 0
-for(i = tamanhoAlgarismo; i>=0; i--){
-	valorAtual = valorAlgarismo(algarismo[i]);
-	if(valorAtual >= valorAnterior){
-		soma += valorAtual;
+let resultado;
+function converter(event){
+	event.preventDefault();
+	let algarismo = document.querySelector("#romano").value;
+	if(pattern.test(algarismo)){
+		alert("Algarismo inválido");
 	}else{
-		soma -= valorAtual;
+		resultado = calcularDecimal(algarismo);
+		valorConvertido.innerHTML = resultado;
 	}
-	valorAnterior = valorAtual;
 }
-console.log(soma);
+
+function calcularDecimal(algarismo){
+	let tamanhoAlgarismo = algarismo.length - 1;
+	let i;
+	let valorAnterior = 0;
+	let soma = 0
+	for(i = tamanhoAlgarismo; i>=0; i--){
+		valorAtual = valorAlgarismo(algarismo[i]);
+		if(valorAtual >= valorAnterior){
+			soma += valorAtual;
+		}else{
+			soma -= valorAtual;
+		}
+		valorAnterior = valorAtual;
+	}
+	return soma;	
+}
+
 
 
 function valorAlgarismo(letra){
